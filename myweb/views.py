a252352cd9cead98f007ef5fffac8c75f75f87ef
nitin singh <nitinsinghb4u@gmail.com>
2020-08-11 23:41:37 +0530

    'second'

--- myweb/views.py
+++ myweb/views.py
@@ -6,17 +6,5 @@ from django.http import HttpResponse
 # Create your views here.
 def web(request):
     return render(request,"myweb/web.html")
-def regs(request):
-    fname =request.POST['fn']
-    lname =request.POST["ln"]
-    email=request.POST["email"]
-    phNo=request.POST["phoneNo"]
-    s=open("lgr.txt",'a')
 
-
-    s.write(fname+","+lname+","+phNo+","+email+"\n")
-
-    s.close()
-
-    return render(request,"myweb/form.html")
     
--- siteproject/urls.py
+++ siteproject/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('myweb',include("myweb.urls")),
+    path('',include("myweb.urls")),
     path('admin/', admin.site.urls),
+    
 ]
