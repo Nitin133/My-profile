fefaf68900e46d2cb51e1ccdb5bd6a2a300b1488
nitin singh <nitinsinghb4u@gmail.com>
2020-08-11 18:30:30 +0530

    Ready to heroku this sucker in the face.

--- siteproject/settings.py
+++ siteproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'ccataj)r@k9*(*!mi)itv8r$5)vtfnhn-)g=d$$z%hk)sa+h)1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
